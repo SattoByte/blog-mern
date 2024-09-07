77825e1d92e591e1bd1ac22a77e7b2725389ff4f
SattoByte <sattobyte@gmail.com>
2024-09-06 21:00:29 -0400

    second commit: tested webtokens OK, connected to DB is OK., listen PORT is OK.

--- index.js
+++ index.js
@@ -1,18 +1,43 @@
 import express from 'express';
+import jwt from 'jsonwebtoken'
+import mongoose from 'mongoose';
+
+mongoose
+  .connect('mongodb+srv://admin:wwwwww@cluster0.6oxue.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+  .then(() => console.log('DB ok'))
+  .catch((err) => console.log('DB Error', err))
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Working!++');
-  //получаем get(req)запрос и возвращяем res.
+  //getting get(req) request to get response
 });
 
+app.post('/auth/login', (req, res) => {
+  console.log('req: ', req.body); //testint login and password here
 
+  if (req.body.email === 'test@mail.com') {
+    const token = jwt.sign({
+      email: req.body.email,
+      fullName: 'Neo',
+    },
+      'secret 123',
+    ); //generate token
+  }
+
+  res.json({ //checking POST request to /auth/login to get answer from server
+    success: true,
+    token,
+  });
+});
 
 app.listen(4444, (err) => {
   if (err) {
     return console.log(err)
   }
-  //слушаем порт 4444 и если нету ошибки то пишем в консоль ОК!
+  //listen to port 4444 and if there is no error, write OK in the console!
   console.log('Server OK!');
 });
